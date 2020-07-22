5336dc760af8bc0f0989200276545e6f778d6cd1
TheCats100 <m-g-152@outlook.fr>
2020-07-22 16:54:54 +0200

    feat(artists): data back on artists list

--- src/components/artists/Artists.jsx
+++ src/components/artists/Artists.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+
+import './artists.css'
 
 function Artists() {
+  const [allArtists, setAllArtists] = useState([]);
+
+  useEffect(() => {
+    Axios.get('http://localhost:8000/artists')
+      .then((response) => response.data)
+      .then((data) => setAllArtists(data))
+  })
+
   return (
-    <div>
-      <p>Hello from Artists</p>
+    <div className="block">
+      {allArtists.map((artists) => {
+        return (
+        <p>{artists.name}</p>
+        )
+      })}
     </div>
   )
 };
