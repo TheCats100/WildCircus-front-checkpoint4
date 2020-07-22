1c890e35a246408a43fdb1a34edfc4b717d8af4f
TheCats100 <m-g-152@outlook.fr>
2020-07-22 16:29:25 +0200

    feat(component): router and intro

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -14,13 +14,13 @@ import Contact from '../contact/Contact';
 import Performances from '../performances/Performances';
 import Login from '../log/Login';
 import Register from '../log/Register';
-import Accueil from '../home/Acceuil';
+import Accueil from './Accueil';
 import Representation from '../representation/Representation';
 
 import './home.css';
 
 function Home() {
-  
+
   function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
   }
@@ -28,36 +28,38 @@ function Home() {
   return (
     <Router >
       <div className="navBar">
-        <span className="burgerMenu" onClick={() => openNav()}>&#9776; open</span>
+        <span className="burgerMenu" onClick={() => openNav()}>&#9776;</span>
         <NavBar />
-        <Link to="/">WildCircus</Link>
-        <Link to="/login">Se connecter ?</Link>
+        <Link to="/" className="titleWild">WildCircus</Link>
+        <Link to="/login" className="navbarConnect">Se connecter ?</Link>
       </div>
       <Switch>
-        <Route exact path="/">
-          <Accueil />
-        </Route>
-        <Route exact path="/artists">
-          <Artists />
-        </Route>
-        <Route exact path="/casino">
-          <Casino />
-        </Route>
-        <Route exact path="/contact">
-          <Contact />
-        </Route>
-        <Route exact path="/performances">
-          <Performances />
-        </Route>
-        <Route exact path="/login">
-          <Login />
-        </Route>
-        <Route exact path="/register">
-          <Register />
-        </Route>
-        <Route exact path="/representations">
-          <Representation />
-        </Route>
+        <div id="componentsBlock">
+          <Route exact path="/">
+            <Accueil />
+          </Route>
+          <Route exact path="/artists">
+            <Artists />
+          </Route>
+          <Route exact path="/casino">
+            <Casino />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+          <Route exact path="/performances">
+            <Performances />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/register">
+            <Register />
+          </Route>
+          <Route exact path="/representations">
+            <Representation />
+          </Route>
+        </div>
       </Switch>
     </Router>
   )
