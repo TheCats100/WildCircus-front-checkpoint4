dcfa33467ec59c5590ae3497ceee93197988bb93
TheCats100 <m-g-152@outlook.fr>
2020-07-23 10:10:09 +0200

    feat(auth): login token without reduce, css

--- src/components/log/Login.jsx
+++ src/components/log/Login.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Axios from 'axios';
+
+import './login.css';
 
 function Login() {
+  const [user, setUser] = useState({})
   return (
-    <div>
-      <p>Hello from Login</p>
+    <div className="block">
+      <form className="loginForm" onSubmit={(event) => {
+        event.preventDefault();
+        Axios.post('http://localhost:8000/auth/login', { ...user })
+          .catch((err) => console.log(err))
+          .then((res) => res.data)
+          .then((data) => console.log(data))
+      }}>
+        <label>
+          Email
+          <input type="text" name="email_login" required="1" onChange={(e) => setUser({...user, email: e.target.value})}/>
+        </label>
+        <label>
+          Password
+          <input type="password" name="password_login" required="1" onChange={(e) => setUser({...user, password: e.target.value})}/>
+        </label>
+        <button type="submit" className="loginButton">Confirm</button>
+      </form>
+      <Link to="/register">S'inscrire</Link>
     </div>
   )
 };
